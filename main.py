309009232d9e78cc761f7f48fff92dd469b5650f
terryking0711 <128213469+terryking0711@users.noreply.github.com>
2025-02-04 23:39:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,15 @@ df = pd.read_csv(r"fc mobile player prices.csv")
 # 爬取的目標URL
 url_initial = 'https://renderz.app/24/player/'
 
-while True:
-    # 獲取當前時間，並格式化作為新列名
-    current_time = str(datetime.now().strftime("%Y-%m-%d %H:%M"))
-    print(current_time)    
-    prices = []
-    for player,code in zip(df["player name"],df['code']):
-        url = url_initial + str(code)
-        market_value = str(get_player_market_value(url))
-        prices.append(market_value)
-    # 將 prices 列表中的值賦給 df[current_time] 列
-    df[current_time] = prices
-    df.to_csv(r"diagrame_analsis.py", index=False)
-    print("Prices updated.")
+# 獲取當前時間，並格式化作為新列名
+current_time = str(datetime.now().strftime("%Y-%m-%d %H:%M"))
+print(current_time)    
+prices = []
+for player,code in zip(df["player name"],df['code']):
+    url = url_initial + str(code)
+    market_value = str(get_player_market_value(url))
+    prices.append(market_value)
+# 將 prices 列表中的值賦給 df[current_time] 列
+df[current_time] = prices
+df.to_csv(r"diagrame_analsis.py", index=False)
+print("Prices updated.")
