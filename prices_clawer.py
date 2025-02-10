b0b97e6f2aa1e4b23d66945fdc94dce3e371dd19
terryking0711 <128213469+terryking0711@users.noreply.github.com>
2025-02-10 17:11:27 +0800

    Update prices_clawer.py

--- prices_clawer.py
+++ prices_clawer.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://renderz.app/24/player/30902460'
 def get_player_market_value(url):
     # 發送 GET 請求取得網頁內容
     response = requests.get(url)
@@ -10,6 +9,12 @@ def get_player_market_value(url):
     soup = BeautifulSoup(response.text, 'html.parser')
 
     # 查找目標數據
-    market_value = soup.find('div', class_='market-data--value flex items-center space-x-2 svelte-18han0u').find('span').text
+    # market_value = soup.find('div', class_='market-data--value flex items-center space-x-2 svelte-18han0u').find('span').text
+    market_value = soup.find('div', class_='market-data--value flex items-center space-x-2 svelte-1kdaqxj').find('span').text
 
     return market_value
+
+if __name__ == '__main__':
+    url = 'https://renderz.app/24/player/30902460'
+    market_value = get_player_market_value(url)
+    print(market_value)
