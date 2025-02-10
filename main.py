1f9de5ee29839e6f628f8ad742990c0d69424593
terryking0711 <128213469+terryking0711@users.noreply.github.com>
2025-02-10 17:14:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,11 @@ import re
 # 讀取CSV檔案
 df = pd.read_csv(r"fc_mobile_player_prices.csv")
 
-def remove_leading_zeros(s: str) -> str:
-    return re.sub(r'\b0+(?!0)(\d+)\b', r'\1', s)
-
 # 爬取的目標URL
 url_initial = 'https://renderz.app/24/player/'
 
 # 獲取當前時間，並格式化作為新列名
 current_time = str(datetime.now().strftime("%Y-%m-%d %H:%M"))
-current_time = remove_leading_zeros(current_time)
 print(current_time)    
 prices = []
 for player,code in zip(df["player name"],df['code']):
